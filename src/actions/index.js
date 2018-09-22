028a1c5fdf37cdcfa5e83a992375b1c377198581
Pierre Charpentier <charpentier.pierre@outlook.com>
2018-09-22 15:17:22 +0200

    Create simple action ADD_REQUIREMENT

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+let nextRequirementId = 0;
+
+export const SeverityLevels = {
+    'COULD': 'could',
+    'MUST': 'must',
+    'NICE': 'nice',
+    'SHOULD': 'should',
+    'WONT': 'won\'t'
+}
+
+export const addRequirement = ({ description, severityLevel }) => ({
+    type: 'ADD_REQUIREMENT',
+    id: nextRequirementId++,
+    description: description,
+    severityLevel: severityLevel
+})
