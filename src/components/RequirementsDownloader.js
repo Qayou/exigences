3f39816e90918b6d61d20fbc4e8bf78070835f9d
Pierre Charpentier <charpentier.pierre@outlook.com>
2018-09-22 15:19:47 +0200

    Create component to download stored requirements

---
+++ src/components/RequirementsDownloader.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+
+import { SeverityLevels } from '../actions'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => ({
+    requirements: state.requirements
+})
+
+// Download the requirements as a plain text file
+const downloadRequirements = requirements => {
+    const fileLink = document.createElement("a")
+    const text = requirements.map(requirement => `* ${requirement.description} / ${SeverityLevels[requirement.severityLevel]}\r\n`)
+    const file = new Blob(text, {type: 'text/plain'})
+    fileLink.href = URL.createObjectURL(file)
+    fileLink.download = "requirements.txt"
+    fileLink.click()
+}
+
+const RequirementsDownloader = ({ requirements }) => (
+    <Button onClick={() => { downloadRequirements(requirements) }}>Download requirements</Button>
+)
+
+export default connect(
+    mapStateToProps
+)(RequirementsDownloader)
