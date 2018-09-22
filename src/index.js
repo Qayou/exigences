8597b761863d9fdbfc41445590aed900d9f83bc3
Pierre Charpentier <charpentier.pierre@outlook.com>
2018-09-22 15:17:02 +0200

    Add redux store and bootstrap style to the app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import rootReducer from "./reducers";
+import App from "./components/App";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
